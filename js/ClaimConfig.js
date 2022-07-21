66049fcaa7d1ecb9b529e99437247a606a617bf3
dapaopro <102985732+dapaopro@users.noreply.github.com>
2022-07-21 15:03:24 +0800

    Update ClaimConfig.js

--- js/ClaimConfig.js
+++ js/ClaimConfig.js
@@ -2,6 +2,6 @@ var ClaimContract = "0x77031a4b64ec1caed3944d2a0fb1c8292522a43d";
 var ChainId = 128;
 var ChainIdError = "请连接火币链钱包地址";
 var NFTContract = "0x6029852FbB3013983c5B090356c6157CF459Ad19";
-var NFTHolder = 0x6029852FbB3013983c5B090356c6157CF459Ad19;
+var NFTHolder = 10;
 var ZeroAddress = "0x0000000000000000000000000000000000000000";
 var FinanceAddress = "0xAa01E8Bcf932aE11019A0135930c0d27818F3774";
